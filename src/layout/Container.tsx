c7c57b7a55b0a422cbe88d94df1c97c49e5e39ed
NerdScriptX <runyshy@gmail.com>
2024-07-09 19:13:23 +0500

    initial commit

---
+++ src/layout/Container.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+type ContainerProps = {
+  children: ReactNode;
+  variant?: "full-bleed" | "large" | "narrow";
+};
+
+export default function Container({
+  children,
+  variant = "full-bleed",
+}: ContainerProps) {
+  return <div className={`container ${variant && variant}`}>{children}</div>;
+}
